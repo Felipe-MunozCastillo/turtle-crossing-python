590ab271a3b626e23abe62e9f86e20fc2b2d22c8
Felipe Muñoz <fmunoz.acastillo@gmail.com>
2024-01-18 20:34:01 -0300

    clase Scoreboard revisada

--- scoreboard.py
+++ scoreboard.py
@@ -1,9 +1,13 @@
+"""Importacion de Turtle para que se 
+muestre el puntaje en pantalla"""
 from turtle import Turtle
 
 FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
+    """Clase encargada de manejar el puntaje del jugador."""
+
     def __init__(self):
         super().__init__()
         self.level = 1
@@ -13,13 +17,16 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
+        """Metodo que actualiza la pantalla con los datos actuales del puntaje."""
         self.clear()
         self.write(f"Level: {self.level}", align="left", font=FONT)
 
     def increase_level(self):
+        """Metodo para aumentar el puntaje del jugador."""
         self.level += 1
         self.update_scoreboard()
 
     def game_over(self):
+        """Metodo para mostrar mensaje una vez que el jugador pierde."""
         self.goto(0, 0)
-        self.write(f"GAME OVER", align="center", font=FONT)
+        self.write("GAME OVER", align="center", font=FONT)
