c9ec7c51809c467acdabf872f6649c2a290b997c
Felipe Muñoz <fmunoz.acastillo@gmail.com>
2024-01-18 20:28:32 -0300

    clase Player revisada

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+"""Libraria turtle necesaria para la creacion del jugador."""
 from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
@@ -6,6 +7,9 @@ FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
+    """Clase encargada de la creacion de la tortuga
+    manipulada por el jugador"""
+
     def __init__(self):
         super().__init__()
         self.color("black")
@@ -15,9 +19,11 @@ class Player(Turtle):
         self.goto(STARTING_POSITION)
 
     def move(self):
+        """Metodo de avance del jugador."""
         self.forward(MOVE_DISTANCE)
 
     def player_crossed(self):
+        """Metodo para detectar si el usuario cruzo o no la pantalla."""
         if self.ycor() >= FINISH_LINE_Y:
             self.goto(STARTING_POSITION)
             return True
